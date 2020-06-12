9c0c463d15c48ef340395f0c85997d49afbd82dc
Yujingyi <349758699@qq.com>
2020-06-13 00:27:29 +0800

    update

---
+++ src/Balanced Binary Tree.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isBalanced(self, root: TreeNode) -> bool:
+        if root == None:
+            return True
+
+        if abs(self.depth(root.left) - self.depth(root.right)) <= 1:
+            return self.isBalanced(root.left) and self.isBalanced(root.right)
+        else:
+            return False
+
+    def depth(self, root):
+        if root == None:
+            return 0
+        return max(self.depth(root.left), self.depth(root.right)) + 1
