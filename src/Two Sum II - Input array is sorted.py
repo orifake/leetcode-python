eb9ccdb82112bb3b72a6fc38f0dc0b9257621d7a
Yujingyi <349758699@qq.com>
2020-06-20 00:03:05 +0800

    update

---
+++ src/Two Sum II - Input array is sorted.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        h = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement not in h:
+                h[num] = i
+            else:
+                return [h[complement] + 1, i + 1]
+
+
+t = Solution()
+print(t.twoSum([2, 7, 11, 15], 9))
