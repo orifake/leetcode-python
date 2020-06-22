f5206d2799594a3141e314f9d1136a2802e64ee4
Yujingyi <349758699@qq.com>
2020-06-23 00:19:21 +0800

    update

---
+++ src/Remove Linked List Elements.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeElements(self, head: ListNode, val: int) -> ListNode:
+        if not head:
+            return None
+
+        dummy = ListNode(0)  #头节点
+        dummy.next = head
+        preNode = dummy  #当前节点的上一个节点
+        currentNode = dummy.next
+        while currentNode is not None:
+            if currentNode.val == val:
+                preNode.next = currentNode.next
+                currentNode = preNode.next
+            else:
+                preNode = preNode.next
+                currentNode = currentNode.next
+
+        return dummy.next
