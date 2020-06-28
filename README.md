c72ccd51fa11298410c04bdc19fc75e8a2a229ff
Yujingyi <349758699@qq.com>
2020-06-28 23:47:34 +0800

    update

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 ## Math
-7 Reverse Integer 
+7 Reverse Integer
+9 Palindrome Number
+13 Roman to Integer
 67 Add Binary
+
+## String
+14 Longest Common Prefix
 
 ## Hash Table
 1 Two Sum
--- src/Longest Common Prefix.py
+++ src/Longest Common Prefix.py
@@ -2,14 +2,26 @@ from typing import List
 
 
 class Solution:
+    # def longestCommonPrefix(self, strs: List[str]) -> str:
+    #     prefix = []
+    #     for x in zip(*strs):
+    #         if len(set(x)) == 1:
+    #             prefix.append(x[0])
+    #         else:
+    #             break
+    #     return "".join(prefix)
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        prefix = []
-        for x in zip(*strs):
-            if len(set(x)) == 1:
-                prefix.append(x[0])
-            else:
-                break
-        return "".join(prefix)
+        if len(strs) == 0:
+            return ""
+
+        prefix = strs[0]
+        for i in range(1, len(strs)):
+            while strs[i].find(prefix) != 0:
+                prefix = prefix[:-1]
+
+                if len(prefix) == 0:
+                    return ""
+        return prefix
 
 
 t = Solution()
