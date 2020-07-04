86cdb04e8d9549fc6e896fd587858409b131ed98
Yujingyi <349758699@qq.com>
2020-07-05 00:37:34 +0800

    :tada:

---
+++ src/Range Sum Query - Immutable.py
@@ -0,0 +1,13 @@
+class NumArray:
+    def __init__(self, nums: List[int]):
+        self.sums = [0] * (len(nums) + 1)
+        for i in range(len(nums)):
+            self.sums[i + 1] = self.sums[i] + nums[i]
+
+    def sumRange(self, i: int, j: int) -> int:
+        return self.sums[j + 1] - self.sums[i]
+
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(i,j)
