d43379e001479b71c6d189a2d1b7f8b5f85b1e99
Yujingyi <349758699@qq.com>
2020-07-06 22:58:36 +0800

    :tada:

---
+++ src/Intersection of Two Arrays II.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        result = []
+        nums1.sort()
+        nums2.sort()
+        i = j = 0
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] == nums2[j]:
+                result.append(nums1[i])
+                i += 1
+                j += 1
+            elif nums1[i] < nums2[j]:
+                i += 1
+            elif nums1[i] > nums2[j]:
+                j += 1
+        return result
+
+
+t = Solution()
+print(t.intersect([4, 9, 5], [9, 4, 9, 8, 4]))
