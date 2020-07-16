b1fa11f3fa37fd40c7ff3bbe29e659695de5e75b
Yujingyi <349758699@qq.com>
2020-07-17 00:01:50 +0800

    :tada:

---
+++ src/Island Perimeter.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        if not grid:
+            return 0
+
+        ans = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
+                    ans += 4
+                    if i > 0 and grid[i - 1][j] == 1:
+                        ans -= 2
+                    if j > 0 and grid[i][j - 1] == 1:
+                        ans -= 2
+        return ans
