1c198d7aead8f8e6a0e62c4094d93638c8abd6d6
Yujingyi <349758699@qq.com>
2020-07-23 21:25:16 +0800

    :tada:

---
+++ src/Maximum Depth of N-ary Tree.py
@@ -0,0 +1,18 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+
+class Solution:
+    def maxDepth(self, root: 'Node') -> int:
+        if root == None:
+            return 0
+
+        if not root.children:
+            return 1
+
+        return max(self.maxDepth(child) for child in root.children) + 1
