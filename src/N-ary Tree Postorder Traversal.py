92c7182a666bd1cc72eb5b1e3fe13c6bcd2a2da3
Yujingyi <349758699@qq.com>
2020-07-26 22:59:54 +0800

    :tada:

---
+++ src/N-ary Tree Postorder Traversal.py
@@ -0,0 +1,18 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        ans = []
+        if not root:
+            return ans
+        for child in root.children:
+            ans.extend(self.postorder(child))
+        ans.append(root.val)
+        return ans
