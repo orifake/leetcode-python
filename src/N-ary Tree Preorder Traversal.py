d7306a21aee9ef39f8191c028ad570dc157f49ee
Yujingyi <349758699@qq.com>
2020-07-26 22:57:33 +0800

    :tada:

---
+++ src/N-ary Tree Preorder Traversal.py
@@ -0,0 +1,18 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+        ans = []
+        if not root:
+            return ans
+        ans.append(root.val)
+        for child in root.children:
+            ans.extend(self.preorder(child))
+        return ans
