d2dcf6dd39de5fe95f2902570e2cd1a7860ae15c
Yujingyi <349758699@qq.com>
2020-08-01 22:47:45 +0800

    :tada:

---
+++ src/Non-decreasing Array.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def checkPossibility(self, nums: List[int]) -> bool:
+        count = 0
+        for i in range(1, len(nums)):
+            if nums[i] < nums[i - 1]:
+                count += 1
+                if i + 1 < len(nums) and i - 2 >= 0:
+                    if nums[i - 1] > nums[i + 1] and nums[i - 2] > nums[i]:
+                        return False
+            if count > 1:
+                return False
+
+        return True
+
+
+t = Solution()
+print(t.checkPossibility([4, 2, 1]))
