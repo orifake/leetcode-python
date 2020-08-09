dce23df1afe38b2a787b0255d2078770699ef7a9
Yujingyi <349758699@qq.com>
2020-08-09 16:37:13 +0800

    :tada:

---
+++ src/Find Smallest Letter Greater Than Target.py
@@ -0,0 +1,12 @@
+from typing import List
+import bisect
+
+
+class Solution:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        index = bisect.bisect(letters, target)
+        return letters[index % len(letters)]
+
+
+t = Solution()
+print(t.nextGreatestLetter(["c", "f", "j"], "k"))
