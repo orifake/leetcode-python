62f7a03dda783c20cd3257f5410e4e9c9e61de27
Yujingyi <349758699@qq.com>
2020-08-19 23:59:31 +0800

    :tada:

---
+++ src/Peak Index in a Mountain Array.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def peakIndexInMountainArray(self, A: List[int]) -> int:
+        lo, hi = 0, len(A) - 1
+        while lo < hi:
+            mi = int((lo + hi) / 2)
+            if A[mi] < A[mi + 1]:
+                lo = mi + 1
+            else:
+                hi = mi
+        return lo
