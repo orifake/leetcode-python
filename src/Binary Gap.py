ac585e1444cadb773cab7bb4a748ebe576a039ae
Yujingyi <349758699@qq.com>
2020-08-21 23:09:56 +0800

    :tada:

---
+++ src/Binary Gap.py
@@ -0,0 +1,13 @@
+class Solution:
+    def binaryGap(self, N: int) -> int:
+        last = None
+        ans = 0
+        for i in range(32):
+            if (N >> i) & 1:
+                if last is not None:
+                    ans = max(ans, i - last)
+                last = i
+        return ans
+
+t = Solution()
+print(t.binaryGap(8))
