94eefcccdaea9cf72578e6e1c2411645bf7f0d04
orifake <349758699@qq.com>
2020-09-01 23:22:28 +0800

    :tada:

---
+++ src/Sort Array By Parity II.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def sortArrayByParityII(self, A: List[int]) -> List[int]:
+        j = 1
+        for i in range(0, len(A), 2):
+            if A[i] % 2:
+                while A[j] % 2:
+                    j += 2
+                A[i], A[j] = A[j], A[i]
+        return A
+
+
+t = Solution()
+print(t.sortArrayByParityII([4, 5, 2, 7]))
