79a096cffeb5ab25d588ffbcfd1599927c37ea86
orifake <349758699@qq.com>
2020-09-02 23:31:11 +0800

    :tada:

---
+++ src/Unique Email Addresses.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def numUniqueEmails(self, emails: List[str]) -> int:
+        seen = set()
+        for email in emails:
+            local, domain = email.split('@')
+            if '+' in local:
+                local = local[:local.index('+')]
+            seen.add(local.replace('.', '') + '@' + domain)
+        return len(seen)
+
+
+t = Solution()
+print(
+    t.numUniqueEmails([
+        "test.email+alex@leetcode.com", "test.e.mail+bob.cathy@leetcode.com",
+        "testemail+david@lee.tcode.com"
+    ]))
