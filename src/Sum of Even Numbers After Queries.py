3e69c7844d0499e8e9ce0ec23c63024d55e57e69
orifake <349758699@qq.com>
2020-09-09 23:41:00 +0800

    :tada:

---
+++ src/Sum of Even Numbers After Queries.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def sumEvenAfterQueries(self, A: List[int],
+                            queries: List[List[int]]) -> List[int]:
+        S = sum(x for x in A if x % 2 == 0)
+        ans = []
+
+        for x, k in queries:
+            if A[k] % 2 == 0: S -= A[k]
+            A[k] += x
+            if A[k] % 2 == 0: S += A[k]
+            ans.append(S)
+
+        return ans
+
+
+t = Solution()
+print(t.sumEvenAfterQueries([1, 2, 3, 4], [[1, 0], [-3, 1], [-4, 0], [2, 3]]))
