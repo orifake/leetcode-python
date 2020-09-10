11cdb1605ac7fdcf440d08f9f661a4e8945a5ab5
orifake <349758699@qq.com>
2020-09-10 22:40:40 +0800

    :tada:

---
+++ src/Add to Array-Form of Integer.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def addToArrayForm(self, A: List[int], K: int) -> List[int]:
+        A[-1] += K
+        for i in range(len(A) - 1, -1, -1):
+            carry, A[i] = divmod(A[i], 10)
+            if i:
+                A[i - 1] += carry
+        if carry:
+            A = list(map(int, str(carry))) + A
+        return A
+
+
+t = Solution()
+print(t.addToArrayForm([2, 1, 5], 806))
