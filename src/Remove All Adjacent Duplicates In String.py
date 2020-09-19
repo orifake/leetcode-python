fe1b3d256d8427aab01275ff64e5efbc854b778f
orifake <349758699@qq.com>
2020-09-19 23:02:30 +0800

    :tada:

---
+++ src/Remove All Adjacent Duplicates In String.py
@@ -0,0 +1,13 @@
+class Solution:
+    def removeDuplicates(self, S: str) -> str:
+        stack = []
+        for s in S:
+            if stack and s == stack[-1]:
+                stack.pop()
+                continue
+            stack.append(s)
+        return "".join(stack)
+
+
+t = Solution()
+print(t.removeDuplicates("abbaca"))
