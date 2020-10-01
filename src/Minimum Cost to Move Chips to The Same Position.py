89ae04794ab33a23939ab491ac5a0d6d8cb5e0f3
orifake <349758699@qq.com>
2020-10-01 22:56:16 +0800

    :tada:

---
+++ src/Minimum Cost to Move Chips to The Same Position.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def minCostToMoveChips(self, position: List[int]) -> int:
+        even_cnt = 0
+        odd_cnt = 0
+        for i in position:
+            if i % 2 == 0:
+                even_cnt += 1
+            else:
+                odd_cnt += 1
+        return min(even_cnt, odd_cnt)
+
+
+t = Solution()
+print(t.minCostToMoveChips([2, 2, 2, 3, 3]))
