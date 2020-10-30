70bd69ce359df3c35757a5d48c9cca1ca458930f
orifake <349758699@qq.com>
2020-10-30 23:31:21 +0800

    :tada:

--- src/Implement strStr.py
+++ src/Implement strStr.py
@@ -1,3 +1,32 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        return haystack.find(needle)
+        if not needle:
+            return 0
+        return self.KMP(haystack, needle)
+
+    def KMP(self, text, pattern):
+        prefix = self.getPrefix(pattern)
+        j = -1
+        for i in range(len(text)):
+            while j > -1 and pattern[j + 1] != text[i]:
+                j = prefix[j]
+            if pattern[j + 1] == text[i]:
+                j += 1
+            if j == len(pattern) - 1:
+                return i - j
+        return -1
+
+    def getPrefix(self, pattern):
+        prefix = [-1] * len(pattern)
+        j = -1
+        for i in range(1, len(pattern)):
+            while j > -1 and pattern[j + 1] != pattern[i]:
+                j = prefix[j]
+            if pattern[j + 1] == pattern[i]:
+                j += 1
+            prefix[i] = j
+        return prefix
+
+
+t = Solution()
+print(t.strStr("hello", "ll"))
