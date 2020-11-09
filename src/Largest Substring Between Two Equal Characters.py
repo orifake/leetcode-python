4e6b7ca20cec84364f969e37702e2a8da4f378ad
orifake <349758699@qq.com>
2020-11-10 00:23:28 +0800

    :tada:

---
+++ src/Largest Substring Between Two Equal Characters.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxLengthBetweenEqualCharacters(self, s: str) -> int:
+        result, lookup = -1, {}
+        for i, c in enumerate(s):
+            result = max(result, i - lookup.setdefault(c, i) - 1)
+        return result
+    
+t = Solution()
+print(t.maxLengthBetweenEqualCharacters("abca"))
