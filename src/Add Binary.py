3749f04537221ea75f3a475252360dc8f0779ded
orifake <349758699@qq.com>
2020-11-30 23:37:53 +0800

    :tada:

--- src/Add Binary.py
+++ src/Add Binary.py
@@ -2,15 +2,18 @@ class Solution:
     # def addBinary(self, a: str, b: str) -> str:
     #     return bin(int(a, 2) + int(b, 2))[2:]
     def addBinary(self, a: str, b: str) -> str:
-        result = ''
-        i, j, plus = len(a) - 1, len(b) - 1, 0
-        while i >= 0 or j >= 0 or plus == 1:
-            plus += int(a[i]) if i >= 0 else 0
-            plus += int(b[j]) if j >= 0 else 0
-            result = str(plus % 2) + result
-            i, j, plus = i - 1, j - 1, int(plus / 2)
-
-        return result
+        ans, carry, val = "", 0, 0
+        for i in range(max(len(a), len(b))):
+            val = carry
+            if i < len(a):
+                val += int(a[-(i + 1)])
+            if i < len(b):
+                val += int(b[-(i + 1)])
+            carry, val = divmod(val, 2)
+            ans += str(val)
+        if carry:
+            ans += str(carry)
+        return ans[::-1]
 
 
 t = Solution()
