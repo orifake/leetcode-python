8eda3385a0ff8bd541ec3373feed93f72114b1f3
orifake <349758699@qq.com>
2020-12-06 23:52:50 +0800

    :tada:

--- src/Valid Anagram.py
+++ src/Valid Anagram.py
@@ -1,16 +1,9 @@
+import collections
+
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
-
-        for i, string in enumerate(s):
-            index = t.find(string)
-            if index > -1:
-                t = t[0:index] + t[index + 1:]
-            else:
-                return False
-
-        return True
+        return collections.Counter(s) == collections.Counter(t)
 
 
 t = Solution()
