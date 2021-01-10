c25f20f010c142cb6ca643092e0252a934f33c2d
orifake <349758699@qq.com>
2021-01-10 12:05:21 +0800

    :tada:

--- src/Intersection of Two Arrays.py
+++ src/Intersection of Two Arrays.py
@@ -1,7 +1,27 @@
 from typing import List
 
 
-class Solution:
+class Solution(object):
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        nums1.sort(), nums2.sort()
+        res = []
+
+        it1, it2 = 0, 0
+        while it1 < len(nums1) and it2 < len(nums2):
+            if nums1[it1] < nums2[it2]:
+                it1 += 1
+            elif nums1[it1] > nums2[it2]:
+                it2 += 1
+            else:
+                if not res or res[-1] != nums1[it1]:
+                    res += nums1[it1],
+                it1 += 1
+                it2 += 1
+
+        return res
+
+
+class Solution2:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
         set1 = set(nums1)
         set2 = set(nums2)
