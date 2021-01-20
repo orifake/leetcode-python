cc038a592f8c12d26eed1ccfd110cf461a7b788c
orifake <349758699@qq.com>
2021-01-20 22:55:42 +0800

    :tada:

--- src/Number of Segments in a String.py
+++ src/Number of Segments in a String.py
@@ -1,3 +1,11 @@
 class Solution:
     def countSegments(self, s):
-        return len(s.split())
+        result = int(len(s) and s[-1] != ' ')
+        for i in range(1, len(s)):
+            if s[i] == ' ' and s[i - 1] != ' ':
+                result += 1
+        return result
+
+
+t = Solution()
+print(t.countSegments("Hello, my name is John"))
