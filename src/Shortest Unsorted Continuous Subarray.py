4a0337851566a9499f69936936740dd1bb79f327
orifake <349758699@qq.com>
2021-02-21 00:14:26 +0800

    :tada:

--- src/Shortest Unsorted Continuous Subarray.py
+++ src/Shortest Unsorted Continuous Subarray.py
@@ -3,6 +3,17 @@ from typing import List
 
 class Solution:
     def findUnsortedSubarray(self, nums: List[int]) -> int:
+        n = len(nums)
+        left, right = -1, -2
+        min_from_right, max_from_left = nums[-1], nums[0]
+        for i in range(1, n):
+            max_from_left = max(max_from_left, nums[i])
+            min_from_right = min(min_from_right, nums[n - 1 - i])
+            if nums[i] < max_from_left: right = i
+            if nums[n - 1 - i] > min_from_right: left = n - 1 - i
+        return right - left + 1
+
+    def findUnsortedSubarray2(self, nums: List[int]) -> int:
         sorted_nums = sorted(nums)
         if sorted_nums == nums:
             return 0
@@ -15,6 +26,7 @@ class Solution:
             right -= 1
 
         return right - left + 1
+
 
 t = Solution()
 print(t.findUnsortedSubarray([2, 6, 4, 8, 10, 9, 15]))
